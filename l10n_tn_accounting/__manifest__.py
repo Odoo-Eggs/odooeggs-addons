dea2cb336e29b113778c801231516d0fe1cb1e84
Ahmed F <ahmed.foudhaili@gmail.com>
2022-09-28 22:09:36 +0100

    add l10n_tn accounting

--- l10n_tn_accounting/__manifest__.py
+++ l10n_tn_accounting/__manifest__.py
@@ -1,8 +1,8 @@
-# © 2019 Ahmed Foudhaili <odoo.eggs@gmail.com>
+# © 2019 Ahmed Foudhaili <ahmed.foudhaili@corail-technologie.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    'name': 'Tunisian Banks data',
-    'desc' : 'Tunisian Banks data',
+    'name': 'Tunisian Accounting',
+    'desc' : 'Tunisian accounting chart and tax localization',
     'version': '12.0.1.0.0',
     'license': '',
     'category': 'Localization',
@@ -10,10 +10,11 @@
               'Odoo-Eggs',
     'website': 'https://github.com/Odoo-Eggs/odooeggs-addons',
     'depends': [
-        'account'
+        'account',
+        'l10_tn',
+        'l10_tn_bank'
     ],
     'data': [
-        'data/account_bank_data.xml',
-    ],
+     ],
     'installable': True,
 }
---
+++ l10n_tn_accounting/readme/DESCRIPTION.rst
@@ -0,0 +1,5 @@
+The aim of this project is a better integration of Odoo with Tunisian legislation and business practices.
+
+.. image:: static/description/tn_chart.png
+
+.. image:: static/description/tn_taxes.png
