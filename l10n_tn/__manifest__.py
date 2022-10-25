b925e9a3ffd8f58675e313b9b0a2094effec9f24
Ahmed F <ahmed.foudhaili@gmail.com>
2022-10-25 16:48:52 +0100

    [MIG] l10n_tn: Migration to 15.0

--- l10n_tn/__manifest__.py
+++ l10n_tn/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Tunisian Accounting',
     'desc' : 'Tunisian accounting chart and tax localization',
-    'version': '12.0.1.0.0',
+    'version': "15.0.1.0.0",
     'license': 'AGPL-3',
     'category': 'Localization',
     'author': 'Odoo-Eggs',
